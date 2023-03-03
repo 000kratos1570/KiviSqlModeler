ec20571133a0a924f5ce08d5166e84c0dc6039b9
Tarshin_TA <isip_t.a.tarshin@mpt.ru>
2023-03-03 14:29:27 +0300

    a

--- KiviSqlModeler/KiviSqlModeler/obj/Debug/net6.0-windows/App.g.i.cs
+++ KiviSqlModeler/KiviSqlModeler/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace KiviSqlModeler {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace KiviSqlModeler {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/KiviSqlModeler;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KiviSqlModeler;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace KiviSqlModeler {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             KiviSqlModeler.App app = new KiviSqlModeler.App();
             app.InitializeComponent();
